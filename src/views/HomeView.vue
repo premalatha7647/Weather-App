b7c5b17e0dc6dde516af39fa84b40be22a41d2ac
premalatha <169128822+premalatha8647@users.noreply.github.com>
2024-07-02 14:13:27 +0530

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,8 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <LandingPage/>
   </main>
 </template>
+<script setup lang="ts">
+import LandingPage from '@/components/LandingPage.vue'
+</script>
